8a50bbaf4552df1b1c1512a52ed6c3e4f3331cdf
bayusky <bayusangkaya@gmail.com>
2023-02-01 23:29:41 +0700

    nightly build

---
+++ hostcheck.py
@@ -0,0 +1,91 @@
+import os
+import csv
+import sys
+import json
+from dotenv import load_dotenv
+
+try:
+    import requests
+except Exception:
+    print("No module 'requests' found. Install: pip3 install requests")
+    sys.exit(1) 
+
+load_dotenv()
+try:
+    comm_app = os.environ.get('COMM_APP')            
+except Exception:
+    print("can't read .env, make sure .env configurd properly")
+    sys.exit()
+    
+#open host csv file
+try:
+    csv_file = open('hostlist.csv')
+except OSError:
+    print("File not found, check availability or permission of hostlist.csv")
+
+# set message
+message = "Host status : "
+
+#open csv file
+with csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    line_count = 0
+    for row in csv_reader:
+               
+        #skip header (column name) on first line
+        if line_count == 0 : 
+            message +=f"\n-------------------------------"
+            line_count +=1
+        else :
+            #put data into variable
+            hostname = row[0]
+            ip_addr = row[1]
+             
+            #do ping host 
+            stream = os.popen('ping -c 4 {}'.format(ip_addr)) 
+            
+            #check feedback from host
+            output = stream.read()
+            if '0 received' in output: 
+                message += f"\n{hostname} status: ---OFFLINE--"
+            else: 
+                message += f"\n{hostname} status: Online"
+            line_count += 1
+          
+
+if comm_app == "slack":
+    url = os.environ.get('WEBHOOK')
+    bot_name = os.environ.get('BOT_NAME')
+    channel = os.environ.get('CHANNEL')
+    #emoji_id = ":computer:" 
+    title = "New report!"
+    slack_data = {
+        "username": bot_name,
+        "channel": channel,
+        "attachments": [
+            {
+                "fields": [
+                    {
+                        "title": title,
+                        "value": message,
+                        "short": "false",
+                    }
+                ]
+            }
+        ]
+    }
+    byte_length = str(sys.getsizeof(slack_data))
+    headers = {'Content-Type': "application/json", 'Content-Length': byte_length}
+    response = requests.post(url, data=json.dumps(slack_data), headers=headers)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+elif comm_app=="telegram":
+    TOKEN = os.environ.get('API_TOKEN')
+    chat_id = os.environ.get('CHAT_ID')
+    #hook to bot API
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+    #send messages
+    (requests.get(url).json())
+else:
+    print(".env not configured properly")
+
